90045a1c1fbe217e0487c64feddffa1dabda9d0b
Idris <252732381+Kras-bot@users.noreply.github.com>
2026-02-28 23:21:41 +0000

    Initial commit

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+      <div>Home page</div>
+    </div>
+    
+  )
+}
+
+export default Home
