5eebede46d3d001e48db10ec084f7274ddd7134a
mali3226 <159960677+mali3226@users.noreply.github.com>
2026-02-28 23:48:57 +0000

    test

---
+++ app/about-us/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+
+
+const AboutUs = () => {
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+      <div>About</div>
+      <Button>Button</Button>
+    </div>
+    
+  )
+}
+
+export default AboutUs
